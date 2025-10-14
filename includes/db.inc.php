d0ad5a2a02b75c53c7773f18920bea6bc8a116e4
Nandan <97480644+Nandan-V@users.noreply.github.com>
2025-10-13 19:24:51 -0600

    Update db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,32 +1,9 @@
-   <?php
-       require_once('config.inc.php');
-       function getData($sql, $parameters) {
-        try {
-            $pdo = new PDO(DBCONNSTRING);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            if (!is_array($parameters)) {
-                $parameters = [$parameters];
-            }
-    
-            if (count($parameters) > 0) {
-                $statement = $pdo->prepare($sql);
-                for ($i = 0; $i < count($parameters); $i++) {
-                    $statement->bindValue($i + 1, $parameters[$i]);
-                }
-                $statement->execute();
-                $result = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch only associative arrays
-            } else {
-                $result = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC); // Fetch only associative arrays
-            }
-    
-            $pdo = null; // close db connection
-            return $result; // Return the result
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    }
-       
+<?php
 
-   
-   ?>
+require_once 'includes/config.inc.php';
+try {
+    $pdo = new PDO(DBCONNSTRING);
+} catch (Exception $e) {
+    die("DB Error: " . $e->getMessage());
+}
+?>
